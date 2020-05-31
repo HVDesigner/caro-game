bcd7ae34d5a46ac388767be945006338db9e59d6
hvdesigner <uctonic123@gmail.com>
2020-06-01 02:07:13 +0700

    at home 2:07AM

--- src/components/ServeChat/index.js
+++ src/components/ServeChat/index.js
@@ -1,47 +1,111 @@
 import React from "react";
+import moment from "moment";
+import {
+  useFirestore,
+  useFirestoreCollectionData,
+  useFirestoreDocDataOnce,
+} from "reactfire";
+import firebase from "firebase/app";
 import ExitSVG from "./../../assets/Exit.svg";
 import AppContext from "./../../context/";
 
 function ServeChat() {
-  const { changeRoute } = React.useContext(AppContext);
-  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+  const { changeRoute, state } = React.useContext(AppContext);
+  const [message, setMessage] = React.useState("");
+
+  const serveChatRef = useFirestore().collection("serve-chat");
+
+  const getServeChatRef = useFirestore()
+    .collection("serve-chat")
+    .orderBy("createdAt", "asc");
+  // .limit(3);
+
+  const chatContent = useFirestoreCollectionData(getServeChatRef);
+
+  const addChatText = () => {
+    if (message) {
+      serveChatRef.add({
+        text: message,
+        uid: state.user.uid,
+        createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      });
+      setMessage("");
+    }
+  };
+
   return (
     <div
       className="d-flex flex-column h-100vh w-100vw bg-brown-wood"
       style={{ maxHeight: "100vh" }}
     >
       <div className="flex-fill">
-        <h3 className="text-center">Chat</h3>
+        <h3 className="text-center text-warning text-stroke-carotv mt-2 mb-0">
+          Chat
+        </h3>
       </div>
-      <div className="flex-fill p-2 h-100 w-100" style={{ maxHeight: "100%" }}>
-        <div
-          className="d-flex flex-column h-100 w-100 brown-border rounded shadow p-2 overflow-auto"
-          style={{ backgroundColor: "#f9da7f", maxHeight: "100%" }}
-        >
-          {arr.map((value) => (
-            <div className="d-flex flex-wrap w-100" key={value}>
-              <p className="mb-0 mr-2">
-                <strong>Name:</strong>
-              </p>
-              <p
-                className="m-0 flex-fill w-100"
-                style={{ wordWrap: "break-word" }}
-              >
-                Text1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111
-              </p>
-              <p className="m-0">3 day ago</p>
-            </div>
-          ))}
+      <div
+        className="flex-fill h-100 w-100 position-relative"
+        style={{ maxHeight: "100%" }}
+      >
+        <div className="p-2 position-absolute h-100 w-100">
+          <div
+            className="p-2 d-flex flex-column h-100 w-100 brown-border rounded shadow overflow-auto"
+            style={{ backgroundColor: "#f9da7f", maxHeight: "100%" }}
+          >
+            {chatContent.map((value, key) => {
+              return (
+                <React.Fragment key={key}>
+                  <div className="d-flex w-100">
+                    <User uid={value.uid} />
+                    <div
+                      className="d-flex mr-auto"
+                      style={{
+                        wordWrap: "normal",
+                        overflowWrap: "anywhere",
+                      }}
+                    >
+                      {value.text}
+                    </div>
+                    <small className="m-0 text-right">
+                      {value.createdAt
+                        ? moment(value.createdAt.toDate()).fromNow()
+                        : ""}
+                    </small>
+                  </div>
+                </React.Fragment>
+              );
+            })}
+          </div>
         </div>
       </div>
       <div className="p-2 d-flex flex-fill">
-        <input
-          className="input-carotv-2 text-white text-left w-100"
-          placeholder="Nhập tin nhắn..."
-          type="text"
-        />
+        <form
+          className="w-100"
+          onSubmit={(e) => {
+            e.preventDefault();
+            addChatText();
+          }}
+        >
+          <input
+            className="input-carotv-2 text-white text-left w-100"
+            placeholder="Nhập tin nhắn..."
+            type="text"
+            value={message}
+            onChange={(e) => {
+              setMessage(e.target.value);
+            }}
+          />
+        </form>
         <div className="ml-2">
-          <p className="m-0">Gửi</p>
+          <p
+            className="m-0 text-warning text-stroke-carotv"
+            onClick={() => {
+              addChatText();
+            }}
+            style={{ cursor: "pointer" }}
+          >
+            Gửi
+          </p>
         </div>
       </div>
       <div className="d-flex justify-content-center">
@@ -52,7 +116,7 @@ function ServeChat() {
           onClick={() => {
             changeRoute("dashboard");
           }}
-          className="wood-btn"
+          className="wood-btn mb-2"
         />
       </div>
     </div>
@@ -60,3 +124,15 @@ function ServeChat() {
 }
 
 export default ServeChat;
+
+function User({ uid }) {
+  const getUserRef = useFirestore().collection("users").doc(uid);
+
+  const user = useFirestoreDocDataOnce(getUserRef);
+
+  return (
+    <p className="mb-0 mr-2" style={{ cursor: "pointer" }}>
+      <strong>{user.name.value}:</strong>
+    </p>
+  );
+}
