e03a213ebdce92f31027b44aa3083aed50a7c1dc
hvdesigner <uctonic123@gmail.com>
2020-07-10 23:41:27 +0700

    fix loading overlay

--- src/components/LoadingOverlay/index.js
+++ src/components/LoadingOverlay/index.js
@@ -4,7 +4,7 @@ import { Spinner } from "react-bootstrap";
 function LoadingOverlay() {
   return (
     <div
-      className="w-100vw h-100 position-absolute"
+      className="w-100vw h-100 position-absolute container"
       style={{ zIndex: 9999, backgroundColor: "#48484891", minHeight: "100vh" }}
     >
       <div className="d-flex justify-content-center align-items-center w-100 h-100">
